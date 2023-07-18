a4c9003c63f797d2f87d8199fe3c16702947991f
ANGELA CAMACHO MORENO <128522668+anyelacamacho03@users.noreply.github.com>
2023-07-18 17:54:04 -0400

    Update ejercicio.php

--- ejercicio.php
+++ ejercicio.php
@@ -1,1 +1,22 @@
+<?php
+$tejas de eternit = $_POST['tejas de eternit'];
+$bloquelon = $_POST['bloquelon'];
+$pinturas= $_POST['pintura'];
+$60000 = $_POST['precio de tejas de eternit'];
+$5500= $_POST['precio bloquelon'];
+$500000= $_POST['precio de pinturas'];
 
+
+echo "LAS TEJAS DE ETERNIT DEL EMPLEADO SON: ".$tejas de eternit;
+echo "<br>";
+echo "LOS BLOQUELONES DEL EMPLEADO SON: ".$bloquelon;
+echo "<br>";
+echo "LAS PINTURAS DEL EMPLEADO SON: ".$pintura;
+echo "<br>";
+echo "EL PRECIO : ".$precio de tejas de eternit;
+echo "<br>";
+echo "EL PRECIO : ".$ precio bloquelon;
+echo "<br>";
+echo "EL PRECIO : ".$precio de pinturas;
+echo "<br>";
+?>
