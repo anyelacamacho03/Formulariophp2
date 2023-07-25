374e9deee23d4760b87985b15f02def4f160ee37
ANGELA CAMACHO MORENO <128522668+anyelacamacho03@users.noreply.github.com>
2023-07-25 15:38:22 -0400

    Update ejercicio.php

--- ejercicio.php
+++ ejercicio.php
@@ -5,17 +5,18 @@ $producto3 = $_POST['producto3'];
 $precio1 = $_POST['precio1'];
 $precio2 = $_POST['precio2'];
 $precio3 = $_POST['precio3'];
- $suma=$precio1+$precio2+$precio3
+
+ $suma=$precio1+$precio2+$precio3;
 
 echo "LA SUMA DE LOS TRES PRODUCTOS EQUIVALE A: ".$suma;
 echo "<br>";
 
-$descuento=$suma*0.16
+$descuento=$suma*0.16;
   
 echo "EL DESCUENTO CORREESPONDE A: ".$descuento;
 echo "<br>";
 
-$totalpago=$suma-$descuento
+$totalpago=$suma-$descuento;
   
 echo "EL TOTAL A PAGAR ES: ".$totalpago;
 echo "<br>";
