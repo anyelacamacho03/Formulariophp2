5a55849a24760db3cab158adf6bb6603a74a92bc
ANGELA CAMACHO MORENO <128522668+anyelacamacho03@users.noreply.github.com>
2023-07-25 15:03:51 -0400

    Update productos.php

--- productos.php
+++ productos.php
@@ -1,1 +1,37 @@
+<html>
+ <body> 
+  
+   <form class= "formulario" action= "ejercicio.php" method= "POST" name="productos">
+     <table name="table" border="1" class="table"> 
+        <tr>
+         <td> nombre producto1  </td>
+         <td> <input type="text" name="producto1" class="caja1" >  </td>          
+        </tr>
+          <tr>
+         <td> nombre producto2  </td>
+         <td> <input type="text" name="producto2" class="caja2" >  </td>          
+        </tr>
+          <tr>
+         <td> nombre producto3  </td>
+         <td> <input type="text" name="producto3" class="caja3" >  </td>          
+        </tr>
+       <tr>
+         <td> nombre precio1  </td>
+         <td> <input type="text" name="precio1" class="caja4" >  </td>          
+        </tr>
+          <tr>
+         <td> nombre precio2  </td>
+         <td> <input type="text" name="proecio2" class="caja5" >  </td>          
+        </tr>
+          <tr>
+         <td> nombre precio3  </td>
+         <td> <input type="text" name="precio3" class="caja6" >  </td>          
+        </tr>
 
+       <tr>        
+         <td> <input type="submit" values="enviar" class="boton" >  </td>          
+        </tr>
+     </table>
+   </form>  
+ </body>  
+</html>
